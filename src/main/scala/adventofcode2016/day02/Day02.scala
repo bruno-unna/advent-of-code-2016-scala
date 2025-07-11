f3a6e445a05b53b4494fe4e2afc06c6007feda28
Bruno Unna <bruno.unna@gmail.com>
2025-07-11 21:42:36 +0100

    Implement part 1 of day 2, pass unit test

--- src/main/scala/adventofcode2016/day02/Day02.scala
+++ src/main/scala/adventofcode2016/day02/Day02.scala
@@ -2,9 +2,42 @@ package adventofcode2016.day02
 
 import zio._
 import adventofcode2016.Util
-import scala.util.matching.Regex
 
-def calculateCode(instructions: Seq[String], startingDigit: Int): Int = ???
+extension (digit: Int)
+  def affect(direction: Char): Int =
+    direction match
+      case 'U' =>
+        digit match
+          case top @ (1 | 2 | 3) => top
+          case rest @ _          => rest - 3
+      case 'D' =>
+        digit match
+          case bottom @ (7 | 8 | 9) => bottom
+          case rest @ _             => rest + 3
+      case 'L' =>
+        digit match
+          case left @ (1 | 4 | 7) => left
+          case rest @ _           => rest - 1
+      case 'R' =>
+        digit match
+          case right @ (3 | 6 | 9) => right
+          case rest @ _            => rest + 1
+
+extension (s: String)
+  def toDigit(fromDigit: Int): Int =
+    if s.isEmpty() then fromDigit
+    else
+      val newDigit = fromDigit.affect(s.head)
+      s.substring(1).toDigit(newDigit)
+
+def calculateCode(instructions: Seq[String], startingDigit: Int): Int =
+  val (_, code) =
+    instructions.foldLeft[(Int, Int)]((startingDigit, 0))((t, s) => {
+      val (prevDigit, acc) = t
+      val newDigit = s.toDigit(prevDigit)
+      (newDigit, acc * 10 + newDigit)
+    })
+  code
 
 object Day02 extends ZIOAppDefault:
   def run =
