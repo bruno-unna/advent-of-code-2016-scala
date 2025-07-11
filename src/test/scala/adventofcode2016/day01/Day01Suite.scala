f6f79798b0474333de7757173dc35a8983f5b005
Bruno Unna <bruno.unna@gmail.com>
2025-07-11 18:02:05 +0100

    Write unit tests for day 1

---
+++ src/test/scala/adventofcode2016/day01/Day01Suite.scala
@@ -0,0 +1,54 @@
+package adventofcode2016.day01
+
+import munit.FunSuite
+
+class Day01Suite extends FunSuite:
+  test("example 1"):
+    val startingPosition = Position(0, 0, Heading.North)
+    val trajectory =
+      List(
+        Transition(Rotation.Right, 2),
+        Transition(Rotation.Left, 3)
+      )
+    val newPosition = trace(startingPosition, trajectory).last
+    val distance = newPosition.distanceTo(startingPosition)
+    assertEquals(distance, 5)
+
+  test("example 2"):
+    val startingPosition = Position(0, 0, Heading.North)
+    val trajectory =
+      List(
+        Transition(Rotation.Right, 2),
+        Transition(Rotation.Right, 2),
+        Transition(Rotation.Right, 2)
+      )
+    val newPosition = trace(startingPosition, trajectory).last
+    val distance = newPosition.distanceTo(startingPosition)
+    assertEquals(distance, 2)
+
+  test("example 3"):
+    val startingPosition = Position(0, 0, Heading.North)
+    val trajectory =
+      List(
+        Transition(Rotation.Right, 5),
+        Transition(Rotation.Left, 5),
+        Transition(Rotation.Right, 5),
+        Transition(Rotation.Right, 3)
+      )
+    val newPosition = trace(startingPosition, trajectory).last
+    val distance = newPosition.distanceTo(startingPosition)
+    assertEquals(distance, 12)
+
+  test("example 4"):
+    val startingPosition = Position(0, 0, Heading.North)
+    val trajectory =
+      List(
+        Transition(Rotation.Right, 8),
+        Transition(Rotation.Right, 4),
+        Transition(Rotation.Right, 4),
+        Transition(Rotation.Right, 8)
+      )
+    val intersection = trace(startingPosition, trajectory).findIntersection
+    assert(intersection.isDefined)
+    val distance = intersection.get.distanceTo(startingPosition)
+    assertEquals(distance, 4)
