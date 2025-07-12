ac8333b89c9ea798d1cc3e8d32f86532585724c8
Bruno Unna <bruno.unna@gmail.com>
2025-07-12 16:33:56 +0100

    Write unit tests for part 1 of day 4

---
+++ src/test/scala/adventofcode2016/Day04Suite.scala
@@ -0,0 +1,24 @@
+package adventofcode2016
+
+import munit.FunSuite
+
+class Day04Suite extends FunSuite:
+  val roomNamesList = List(
+    "aaaaa-bbb-z-y-x-123[abxyz]",
+    "a-b-c-d-e-f-g-h-987[abcde]",
+    "not-a-real-room-404[oarel]",
+    "totally-real-room-200[decoy]"
+  )
+  test("validate real rooms"):
+    for i <- 0 to 2 do
+      val maybeRoom = Room.fromString(roomNamesList(i))
+      assert(maybeRoom.isDefined)
+      assert(maybeRoom.get.isValid)
+
+  test("invalidate unreal rooms"):
+    val maybeRoom = Room.fromString(roomNamesList(3))
+    assert(maybeRoom.isDefined)
+    assert(!maybeRoom.get.isValid)
+
+  test("sum sector IDs"):
+    val sumOfSectors = Day04.sumValidSectors(roomNamesList)
