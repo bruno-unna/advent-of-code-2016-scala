9356dc752a60c406bc4c39343b3134797361c0c0
Bruno Unna <bruno.unna@gmail.com>
2025-07-13 00:20:27 +0100

    Implement MD5

--- src/main/scala/adventofcode2016/Day05.scala
+++ src/main/scala/adventofcode2016/Day05.scala
@@ -2,10 +2,19 @@ package adventofcode2016
 
 import zio._
 import adventofcode2016.Util
+import java.security.MessageDigest
+import java.nio.charset.StandardCharsets
 
 object Day05:
 
-  def calculatePassword(doorID: String): String = ???
+  val md = MessageDigest.getInstance("MD5")
+
+  private def md5(s: String): String =
+    val hashBytes = md.digest(s.getBytes(StandardCharsets.UTF_8))
+    hashBytes.map("%02x".format(_)).mkString
+
+  def calculatePassword(doorID: String): String =
+    ???
 
   @main def run =
     val doorID = "ugkcyxxp"
