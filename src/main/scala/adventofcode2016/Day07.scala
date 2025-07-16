dbf75dcf383efbc1f46a27b119ca574225f89f7e
Bruno Unna <bruno.unna@gmail.com>
2025-07-16 21:51:19 +0100

    Implement part 1 of day 7

--- src/main/scala/adventofcode2016/Day07.scala
+++ src/main/scala/adventofcode2016/Day07.scala
@@ -2,10 +2,23 @@ package adventofcode2016
 
 import adventofcode2016.Util
 import zio.*
+import scala.util.matching.Regex
 
 object Day07 extends ZIOAppDefault:
 
-  def supportsTLS(ip: String): Boolean = ???
+  val abbaRE: Regex = """[a-z]*([a-z])((?!\1)[a-z])\2\1[a-z]*""".r
+  val ipBlockRE: Regex = """(?:([a-z]+)(?:\[([a-z]+)\])?)""".r
+
+  def supportsTLS(ip: String): Boolean =
+    val segments = ipBlockRE
+      .findAllMatchIn(ip)
+      .map: m =>
+        val out = Option(m.group(1)).map(abbaRE.matches(_)).getOrElse(false)
+        val in = Option(m.group(2)).map(abbaRE.matches(_)).getOrElse(false)
+        (out, in)
+      .toList
+    segments.map(_._1).exists(_ == true) &&
+    segments.map(_._2).forall(_ == false)
 
   def run =
     for
