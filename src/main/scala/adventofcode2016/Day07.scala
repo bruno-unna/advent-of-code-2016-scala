8959f4a4fe94f424cfec0fc7e2aeb52315a3e369
Bruno Unna <bruno.unna@gmail.com>
2025-07-22 21:11:22 +0100

    Implement second part of day 7 (pass unit tests)

--- src/main/scala/adventofcode2016/Day07.scala
+++ src/main/scala/adventofcode2016/Day07.scala
@@ -6,8 +6,9 @@ import scala.util.matching.Regex
 
 object Day07 extends ZIOAppDefault:
 
-  val abbaRE: Regex = """[a-z]*([a-z])((?!\1)[a-z])\2\1[a-z]*""".r
   val ipBlockRE: Regex = """(?:([a-z]+)(?:\[([a-z]+)\])?)""".r
+  val abbaRE: Regex = """[a-z]*([a-z])((?!\1)[a-z])\2\1[a-z]*""".r
+  val abaRE: Regex = """([a-z])((?!\1)[a-z])\1""".r
 
   def supportsTLS(ip: String): Boolean =
     val segments = ipBlockRE
@@ -20,12 +21,42 @@ object Day07 extends ZIOAppDefault:
     segments.map(_._1).exists(_ == true) &&
     segments.map(_._2).forall(_ == false)
 
+  def supportsSSL(ip: String): Boolean =
+    val segments = ipBlockRE
+      .findAllMatchIn(ip)
+      .map: m =>
+        (Option(m.group(1)), Option(m.group(2)))
+      .toList
+    val supernet = segments
+      .map(_._1)
+      .collect:
+        case Some(value) => value
+      .mkString(" ")
+    val hypernet = segments
+      .map(_._2)
+      .collect:
+        case Some(value) => value
+      .mkString(" ")
+    val matchFound = (0 to supernet.length - 3)
+      .map: index =>
+        val subSupernet = supernet.substring(index)
+        val maybeBab = abaRE
+          .findFirstIn(subSupernet)
+          .map: aba =>
+            List(aba.charAt(1), aba.charAt(0), aba.charAt(1)).mkString
+        maybeBab.map(hypernet.contains(_))
+      .collect:
+        case Some(value) => value
+    matchFound.exists(_ == true)
+
   def run =
     for
       ips <- Util.readStrings("/07.txt")
       tlsSupportCount = ips.count(supportsTLS)
+      sslSupportCount = ips.count(supportsSSL)
       _ = printf(
-        "Day 07\n\tnumber of IP addresses supporting TLS: %d\n",
-        tlsSupportCount
+        "Day 07\n\tnumber of IP addresses supporting TLS: %d\n\tnumber of IP addresses supporting SSL: %d\n",
+        tlsSupportCount,
+        sslSupportCount
       )
     yield ExitCode.success
--- src/test/scala/adventofcode2016/Day07Suite.scala
+++ src/test/scala/adventofcode2016/Day07Suite.scala
@@ -1,23 +1,38 @@
 package adventofcode2016
 
+import adventofcode2016.Day07.*
 import munit.FunSuite
-import adventofcode2016.Day07
-import adventofcode2016.Day07.supportsTLS
 
 class Day07Suite extends FunSuite:
 
-  test("example 1"):
+  test("TLS example 1"):
     val ip = "abba[mnop]qrst"
     assert(supportsTLS(ip))
 
-  test("example 2"):
+  test("TLS example 2"):
     val ip = "abcd[bddb]xyyx"
     assert(supportsTLS(ip) == false)
 
-  test("example 3"):
+  test("TLS example 3"):
     val ip = "aaaa[qwer]tyui"
     assert(supportsTLS(ip) == false)
 
-  test("example 4"):
+  test("TLS example 4"):
     val ip = "ioxxoj[asdfgh]zxcvbn"
     assert(supportsTLS(ip))
+
+  test("SSL example 1"):
+    val ip = "aba[bab]xyz"
+    assert(supportsSSL(ip))
+
+  test("SSL example 2"):
+    val ip = "xyx[xyx]xyx"
+    assert(supportsSSL(ip) == false)
+
+  test("SSL example 3"):
+    val ip = "aaa[kek]eke"
+    assert(supportsSSL(ip))
+
+  test("SSL example 4"):
+    val ip = "zazbz[bzb]cdb"
+    assert(supportsSSL(ip))
