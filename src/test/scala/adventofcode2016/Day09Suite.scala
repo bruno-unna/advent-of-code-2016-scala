4661effdc42315d744fb59883fcabc55b2bc0523
Bruno Unna <bruno.unna@gmail.com>
2025-07-25 17:04:49 +0100

    Write the rest of the unit tests

--- src/test/scala/adventofcode2016/Day09Suite.scala
+++ src/test/scala/adventofcode2016/Day09Suite.scala
@@ -13,3 +13,28 @@ class Day09Suite extends FunSuite:
     val compressed = "ADVENT"
     val decompressed = decompress(compressed)
     assertEquals(decompressed, "ADVENT")
+
+  test("Decompression example 2"):
+    val compressed = "A(1x5)BC"
+    val decompressed = decompress(compressed)
+    assertEquals(decompressed, "ABBBBBC")
+
+  test("Decompression example 3"):
+    val compressed = "(3x3)XYZ"
+    val decompressed = decompress(compressed)
+    assertEquals(decompressed, "XYZXYZXYZ")
+
+  test("Decompression example 4"):
+    val compressed = "A(2x2)BCD(2x2)EFG"
+    val decompressed = decompress(compressed)
+    assertEquals(decompressed, "ABCBCDEFEFG")
+
+  test("Decompression example 5"):
+    val compressed = "(6x1)(1x3)A"
+    val decompressed = decompress(compressed)
+    assertEquals(decompressed, "(1x3)A")
+
+  test("Decompression example 6"):
+    val compressed = "X(8x2)(3x3)ABCY"
+    val decompressed = decompress(compressed)
+    assertEquals(decompressed, "X(3x3)ABC(3x3)ABCY")
