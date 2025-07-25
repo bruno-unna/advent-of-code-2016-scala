f34af4ce2487bed41945fe1f20d1c810b673e40e
Bruno Unna <bruno.unna@gmail.com>
2025-07-25 19:15:35 +0100

    Write unit tests for part 2 of day 9

--- src/test/scala/adventofcode2016/Day09Suite.scala
+++ src/test/scala/adventofcode2016/Day09Suite.scala
@@ -38,3 +38,23 @@ class Day09Suite extends FunSuite:
     val compressed = "X(8x2)(3x3)ABCY"
     val decompressed = decompress(compressed)
     assertEquals(decompressed, "X(3x3)ABC(3x3)ABCY")
+
+  test("Composasble decompression example 1"):
+    val compressed = "(3x3)XYZ"
+    val decompressedLength = composableDecompress(compressed)
+    assertEquals(decompressedLength, 9)
+
+  test("Composasble decompression example 2"):
+    val compressed = "X(8x2)(3x3)ABCY"
+    val decompressedLength = composableDecompress(compressed)
+    assertEquals(decompressedLength, 20)
+
+  test("Composasble decompression example 3"):
+    val compressed = "(27x12)(20x12)(13x14)(7x10)(1x12)A"
+    val decompressedLength = composableDecompress(compressed)
+    assertEquals(decompressedLength, 241920)
+
+  test("Composasble decompression example 4"):
+    val compressed = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
+    val decompressedLength = composableDecompress(compressed)
+    assertEquals(decompressedLength, 445)
