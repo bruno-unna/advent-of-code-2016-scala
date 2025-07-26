b4fe2d913cdd8be70fe2682f05e5516a723c0a87
Bruno Unna <bruno.unna@gmail.com>
2025-07-26 15:29:26 +0100

    Implement the second part to pass unit tests

--- src/main/scala/adventofcode2016/Day09.scala
+++ src/main/scala/adventofcode2016/Day09.scala
@@ -34,7 +34,21 @@ object Day09 extends ZIOAppDefault:
   def composableDecompress(compressed: String): Long =
 
     @tailrec
-    def composableDecompressRec(accLength: Long, compressed: String): Long = ???
+    def composableDecompressRec(accLength: Long, compressed: String): Long =
+      if compressed.isBlank then accLength
+      else
+        compressed.match
+          case MarkerDataRE(prefix, dataLengthStr, repetitionsStr, suffix) =>
+            val dataLength = dataLengthStr.toInt
+            val repetitions = repetitionsStr.toInt
+
+            val expandedSegment = suffix.take(dataLength) * repetitions
+
+            val newSuffix = expandedSegment + suffix.drop(dataLength)
+
+            composableDecompressRec(accLength + prefix.length, newSuffix)
+          case _ =>
+            accLength + compressed.length
 
     composableDecompressRec(0, compressed)
 
--- src/test/scala/adventofcode2016/Day09Suite.scala
+++ src/test/scala/adventofcode2016/Day09Suite.scala
@@ -42,19 +42,19 @@ class Day09Suite extends FunSuite:
   test("Composasble decompression example 1"):
     val compressed = "(3x3)XYZ"
     val decompressedLength = composableDecompress(compressed)
-    assertEquals(decompressedLength, 9)
+    assertEquals(decompressedLength, 9L)
 
   test("Composasble decompression example 2"):
     val compressed = "X(8x2)(3x3)ABCY"
     val decompressedLength = composableDecompress(compressed)
-    assertEquals(decompressedLength, 20)
+    assertEquals(decompressedLength, 20L)
 
   test("Composasble decompression example 3"):
     val compressed = "(27x12)(20x12)(13x14)(7x10)(1x12)A"
     val decompressedLength = composableDecompress(compressed)
-    assertEquals(decompressedLength, 241920)
+    assertEquals(decompressedLength, 241920L)
 
   test("Composasble decompression example 4"):
     val compressed = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
     val decompressedLength = composableDecompress(compressed)
-    assertEquals(decompressedLength, 445)
+    assertEquals(decompressedLength, 445L)
