1729d8b0382ef3af40cf548d95286158018ab957
Bruno Unna <bruno.unna@gmail.com>
2025-08-06 14:15:52 +0100

    Re-implement part 1 of day 5, using ZIO
    The idea of using ZIO in this case was to create batches of fibers
    working in parallel.

---
+++ src/test/scala/adventofcode2016/Day05ZIOSuite.scala
@@ -0,0 +1,17 @@
+package adventofcode2016
+
+import zio.test.{Spec, ZIOSpecDefault, assertTrue}
+
+object Day05ZIOSuite extends ZIOSpecDefault:
+
+  def spec: Spec[Any, Throwable] = suite("Password cracking")(
+    test("calculate the password"):
+      val doorID = "abc"
+      for password <- Day05ZIO.calculatePassword(doorID)
+      yield assertTrue(password == "18f47a30")
+//    ,
+//    test("calculate the second password"):
+//      val doorID = "abc"
+//      for password <- Day05ZIO.calculateSecondPassword(doorID)
+//      yield assertTrue(password == "05ace8e3")
+  )
