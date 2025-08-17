1e73275d8ea77add4d47a2212f34906c83b7d6b6
Bruno Unna <bruno.unna@gmail.com>
2025-08-17 18:37:18 +0100

    Fix the calculation of safety
    When calculating the new state, safety needs to be asses for *both* the
    origin and the destination floors, not just the latter.

--- src/main/scala/adventofcode2016/Day11.scala
+++ src/main/scala/adventofcode2016/Day11.scala
@@ -35,7 +35,7 @@ object Day11:
 
   private def findSolutions(initialState: State, desiredState: State): Solution =
 
-    def isSafe(devices: Seq[Device]): Boolean =
+    def isSafe(devices: Set[Device]): Boolean =
       val generators = devices.collect { case g: Generator => g.element }
       val chips = devices.collect { case c: Chip => c.element }
 
@@ -43,47 +43,20 @@ object Day11:
       else chips.forall(c => generators.contains(c))
 
     def calculateNewState(state: State, destinationNumber: Byte, payload: Payload): Option[State] =
-      val devicesAtOrigin = state.deviceLocations.filter((_, floor) => floor == state.liftFloor).map((device, _) => device).toSeq
-      val devicesAtDestination = state.deviceLocations.filter((_, floor) => floor == destinationNumber).map((device, _) => device).toSeq
-      val untouchedDevices = state.deviceLocations.filterNot(devicesAtOrigin.contains(_)).filterNot(devicesAtDestination.contains(_))
+      val newDeviceLocations = payload.maybeSecond.fold(
+        state.deviceLocations + (payload.first -> destinationNumber)
+      )(
+        d => state.deviceLocations + (payload.first -> destinationNumber) + (d -> destinationNumber)
+      )
 
-      val newDevicesAtDestination = payload.maybeSecond.fold(devicesAtDestination :+ payload.first)(devicesAtDestination :+ payload.first :+ _)
+      val devicesAtDestination = newDeviceLocations.filter((_, floor) => floor == destinationNumber).keys.toSet
+      val devicesAtOrigin = newDeviceLocations.filter((_, floor) => floor == state.liftFloor).keys.toSet
 
-      for
-        newSafeDevicesAtDestination <- Some(newDevicesAtDestination).filter(isSafe)
-        safeDevicesAsMap = newSafeDevicesAtDestination.map(_ -> destinationNumber).toMap
-
-        newDevicesAtOrigin = payload.maybeSecond
-          .fold(devicesAtOrigin.filterNot(_ == payload.first))
-          (d => devicesAtOrigin.filterNot(_ == payload.first).filterNot(_ == d))
-        originDevicesAsMap = newDevicesAtOrigin.map(_ -> state.liftFloor).toMap
-
-        newDevices = untouchedDevices ++ safeDevicesAsMap ++ originDevicesAsMap
-      yield State(destinationNumber, newDevices)
+      if isSafe(devicesAtDestination) && isSafe(devicesAtOrigin) then Some(State(destinationNumber, newDeviceLocations))
+      else None
 
     def findTransitions(state: State): Set[State] =
-      // find the origin floor
-      // find all possible payloads (lift with one or two items)
-      // find the possible destination floors (can be one or two)
-
-      // for each possible payload:
-      //   for each pair of floors (origin, destination):
-      //     calculate a new origin (remove lift and items) and destination (include lift and items)
-      //     maybe create a new State, with the above pair and the rest of the floors (if it's safe)
-      //   yield the newly created maybe state
-      // yield a set with all the maybe created states
-
-      // collect the maybe created states
-
       val originFloorDevices = state.deviceLocations.filter((_, f) => f == state.liftFloor).keys.toSet
-
-      val payloadPairs =
-        for
-          aDevice <- originFloorDevices
-          anotherDevice <- originFloorDevices - aDevice
-        yield Payload.from(aDevice, anotherDevice)
-
-      val payloads = originFloorDevices.map(Payload.from) ++ payloadPairs
 
       val destinationFloorNumbers: Set[Byte] =
         state.liftFloor match
@@ -93,38 +66,42 @@ object Day11:
           case 4 => Set(3)
           case _ => Set.empty
 
-      val maybeStates = for
-        payload <- payloads
-        destinationNumber <- destinationFloorNumbers
-        maybeNewState = calculateNewState(state, destinationNumber, payload)
-      yield maybeNewState
+      val validTransitions = for
+        destination <- destinationFloorNumbers
+        itemsToMove <- originFloorDevices.subsets(1) ++ originFloorDevices.subsets(2)
+        payload = itemsToMove.headOption.fold(Payload(null, None)):
+          head => Payload(head, itemsToMove.tail.headOption)
+        newState <- calculateNewState(state, destination, payload)
+      yield newState
 
-      maybeStates.collect:
-        case Some(state) => state
+      validTransitions
 
-    def reconstructPaths(parents: Map[State, State], desiredState: State): Solution =
-      def loop(current: State): List[State] =
+    def reconstructPath(parents: Map[State, State], endState: State): Solution =
+      @tailrec
+      def loop(current: State, acc: List[State]): List[State] =
         parents.get(current) match
-          case Some(parent) => parent :: loop(parent)
-          case None => List(current)
+          case Some(parent) => loop(parent, current :: acc)
+          case None => current :: acc
 
-      loop(desiredState).reverse
+      loop(endState, Nil)
 
     @tailrec
-    def loop(desiredState: State, queue: Queue[State], seen: Set[State], parents: Map[State, State]): Map[State, State] =
-      if queue.isEmpty then parents
+    def bfs(queue: Queue[State], seen: Set[State], parents: Map[State, State]): Map[State, State] =
+      if queue.isEmpty then
+        parents
       else
         val (state, reducedQueue) = queue.dequeue
-        if state == desiredState then parents
+        if state == desiredState then
+          parents
         else
           val potentialStates = findTransitions(state).diff(seen)
-          val newSeenStates = seen ++ potentialStates
+          val newSeen = seen ++ potentialStates
           val newQueue = reducedQueue.enqueueAll(potentialStates)
-          val newParents = parents ++ potentialStates.map(_ -> state)
-          loop(desiredState, newQueue, newSeenStates, newParents)
+          val newParents = parents ++ potentialStates.map(s => s -> state)
+          bfs(newQueue, newSeen, newParents)
 
-    val parents = loop(desiredState, Queue[State](initialState), Set.empty[State], Map.empty[State, State])
-    reconstructPaths(parents, desiredState)
+    val parents = bfs(Queue(initialState), Set(initialState), Map.empty)
+    reconstructPath(parents, desiredState)
 
   @main
   def main(): Unit =
@@ -142,4 +119,4 @@ object Day11:
 
     val solutions = findSolutions(initialState, desiredState)
 
-    println(s"Part 1: the minimum number of lift operations is ${solutions.length}")
+    println(s"Part 1: the minimum number of lift operations is ${solutions.length - 1}")
