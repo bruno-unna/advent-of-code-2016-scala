afcafc1367458b8f62bc5d0ddc1a5e18c1b32a2e
Bruno Unna <bruno.unna@gmail.com>
2025-09-13 14:47:33 +0100

    Reorder methods

--- src/main/scala/adventofcode2016/Day16.scala
+++ src/main/scala/adventofcode2016/Day16.scala
@@ -2,9 +2,9 @@ package adventofcode2016
 
 object Day16 extends App:
 
-  def calculateChecksum(str: String): String = ???
-
   def fillDisc(size: Int, initState: String): String = ???
+
+  def calculateChecksum(str: String): String = ???
 
   @main def runDay16(): Unit =
     println("Day 16")
