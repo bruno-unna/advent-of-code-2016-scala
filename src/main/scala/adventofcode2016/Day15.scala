6f590664f7a24dc1baa913272d128c6303579205
Bruno Unna <bruno.unna@gmail.com>
2025-09-13 12:40:49 +0100

    Add scaladoc to day 15

--- src/main/scala/adventofcode2016/Day15.scala
+++ src/main/scala/adventofcode2016/Day15.scala
@@ -1,17 +1,55 @@
 package adventofcode2016
 
+/**
+ * Solves Advent of Code 2016, Day 15.
+ */
 object Day15 extends App:
 
-  case class Disc(n: Int, positions: Int, offset: Int):
+  /**
+   * Represents a single disc in the capsule machine.
+   *
+   * @param n The number of the disc, starting from 1.
+   * @param positions The total number of positions the disc can be in.
+   * @param offset The initial position of the disc at time t=0.
+   */
+  private case class Disc(n: Int, positions: Int, offset: Int):
+    /**
+     * Checks if the disc is in the "open" position at a given time.
+     * A disc is open if its final position (n + offset + t) is a multiple of its total positions.
+     *
+     * @param t The time in seconds since the capsule was released.
+     * @return true if the disc is open, false otherwise.
+     */
     def openAtTime(t: Int): Boolean = (n + offset + t) % positions == 0
 
-  case class Machine(discs: Seq[Disc]):
+  /**
+   * Represents the capsule machine with a sequence of discs.
+   *
+   * @param discs The sequence of discs in the machine.
+   */
+  private case class Machine(discs: Seq[Disc]):
+    /**
+     * Checks if all discs in the machine are open at a given time.
+     *
+     * @param t The time in seconds.
+     * @return true if all discs are open, false otherwise.
+     */
     private def openAtTime(t: Int): Boolean = discs.forall(_.openAtTime(t))
 
+    /**
+     * Finds the first non-negative time `t` at which all discs are in the open position.
+     *
+     * This is a brute-force approach that checks each time step starting from 0.
+     *
+     * @return The earliest time at which all discs are open.
+     */
     def calculateRightTime(): Int =
       val timeStream = LazyList.from(0)
       timeStream.find(this.openAtTime).get
 
+  /**
+   * Main entry point for the Day 15 solution.
+   */
   @main def runDay15(): Unit =
     println("Day 15")
 
