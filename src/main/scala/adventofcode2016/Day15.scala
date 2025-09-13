812165abb670ad4fcdb09b43a9f1f8c2e7384fec
Bruno Unna <bruno.unna@gmail.com>
2025-09-13 09:57:50 +0100

    Implement first part of day 15

---
+++ src/main/scala/adventofcode2016/Day15.scala
@@ -0,0 +1,28 @@
+package adventofcode2016
+
+object Day15 extends App:
+
+  case class Disc(n: Int, positions: Int, offset: Int):
+    def openAtTime(t: Int): Boolean = (n + offset + t) % positions == 0
+
+  case class Machine(discs: Seq[Disc]):
+    private def openAtTime(t: Int): Boolean = discs.forall(_.openAtTime(t))
+
+    def calculateRightTime(): Int =
+      val timeStream = LazyList.from(0)
+      timeStream.find(this.openAtTime).get
+
+  @main def runDay15(): Unit =
+    println("Day 15")
+
+    val machine = Machine(Array(
+      Disc(n = 1, positions = 17, offset = 1),
+      Disc(n = 2, positions = 7, offset = 0),
+      Disc(n = 3, positions = 19, offset = 2),
+      Disc(n = 4, positions = 5, offset = 0),
+      Disc(n = 5, positions = 3, offset = 0),
+      Disc(n = 6, positions = 13, offset = 5),
+    ))
+
+    val time = machine.calculateRightTime()
+    println(s"time to push the button: $time")
