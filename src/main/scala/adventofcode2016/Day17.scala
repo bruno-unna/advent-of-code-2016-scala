f0bd7f3115c70a6bedb42aa203e024c47b4e29b9
Bruno Unna <bruno.unna@gmail.com>
2025-09-27 16:25:29 +0100

    Implement part 2, day 17 (tests pass)

--- src/main/scala/adventofcode2016/Day17.scala
+++ src/main/scala/adventofcode2016/Day17.scala
@@ -46,9 +46,31 @@ object Day17:
 
     loop(initialPath, initialQueue)
 
+  def findAllPaths(passcode: String): Set[String] =
+    val origin = Coordinates(0, 0)
+    val destination = Coordinates(3, 3)
+    val initialPath = ""
+
+    def loop(path: String, cell: Coordinates): Set[String] =
+      if cell == destination then Set(path)
+      else
+        val availableMoves = cell.checkMobility(passcode + path)
+        for
+          (direction, coordinates) <- availableMoves
+          newCells <- loop(path + direction, coordinates)
+        yield newCells
+
+    val availableMoves = origin.checkMobility(passcode)
+    availableMoves.flatMap: (direction, coordinates) =>
+      loop(initialPath + direction, coordinates)
+
   @main
   def day17(): Unit =
     val passcode = "qljzarfv"
 
     val steps = findSteps(passcode)
     println(s"Part 1: the minimum path is $steps")
+
+    val allPaths = findAllPaths(passcode)
+    val maxLength = allPaths.map(_.length).max
+    println(s"Part 2: the maximum path length is $maxLength")
