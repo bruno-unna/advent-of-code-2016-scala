ad9bc8f7d8fbb87fc818314f3c4bca2e1ddccddf
Bruno Unna <bruno.unna@gmail.com>
2025-09-28 16:16:55 +0100

    Implement part 1 of day 19

--- src/main/scala/adventofcode2016/Day19.scala
+++ src/main/scala/adventofcode2016/Day19.scala
@@ -1,11 +1,32 @@
 package adventofcode2016
 
+import scala.annotation.tailrec
+import scala.collection.mutable
+
 object Day19:
-  def reduce(elves: Int): Int = ???
+  private case class Elf(presents: Int, neighbour: Int)
+
+  def play(nElves: Int): Int =
+
+    val playfield = mutable.Map[Int, Elf]()
+    0 until nElves foreach (i => playfield.update(i, Elf(1, (i + 1) % nElves)))
+
+    @tailrec
+    def reduce(turn: Int): Int =
+      val elf = playfield(turn)
+      if elf.presents == nElves then // we found the winner
+        turn
+      else // keep playing
+        val victim = playfield(elf.neighbour)
+        playfield.update(turn, elf.copy(presents = elf.presents + victim.presents, neighbour = victim.neighbour))
+        reduce(turn = victim.neighbour)
+
+    reduce(turn = 0) + 1
 
   @main
   def day19(): Unit =
     val elves = 3004953
 
-    val winner = reduce(elves)
-    println(s"Part 1: out of $elves elves, the winner is $winner")
+    val winner = play(elves)
+    println(s"Part 1: out of $elves elves, the winner if elimination is by neighbour is $winner")
+
--- src/test/scala/adventofcode2016/Day19Suite.scala
+++ src/test/scala/adventofcode2016/Day19Suite.scala
@@ -5,6 +5,7 @@ import munit.FunSuite
 class Day19Suite extends FunSuite:
 
   test("5 elves"):
-    val actualWinner = Day19.reduce(5)
+    val actualWinner = Day19.play(nElves = 5)
     val expectedWinner = 3
     assertEquals(actualWinner, expectedWinner)
+
