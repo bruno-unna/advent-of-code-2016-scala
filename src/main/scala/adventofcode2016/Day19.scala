1eaf81f0a2edbe3852ed0758a30d2d0d0baf6bca
Bruno Unna <bruno.unna@gmail.com>
2025-09-30 17:08:30 +0100

    Implement part 2 of day 19

--- src/main/scala/adventofcode2016/Day19.scala
+++ src/main/scala/adventofcode2016/Day19.scala
@@ -24,7 +24,31 @@ object Day19:
     reduce(turn = 1)
 
   def playDiagonally(nElves: Int): Int =
-    ???
+    val playfield = mutable.Map[Int, Elf]()
+    1 to nElves foreach (i => playfield.update(i, Elf(1, (i % nElves) + 1)))
+
+    @tailrec
+    def reduce(turn: Int, nElves: Int, nearMissIdx: Int): Int =
+      if nElves == 1 then turn
+      else
+        val victimIdx = playfield(nearMissIdx).neighbour
+        val victim = playfield(victimIdx)
+        val oldElf = playfield(turn)
+        val newElf = oldElf.copy(presents = oldElf.presents + victim.presents)
+        playfield.update(turn, newElf)
+
+        val oldNearMiss = playfield(nearMissIdx)
+        val newNearMiss = oldNearMiss.copy(neighbour = victim.neighbour)
+        playfield.update(nearMissIdx, newNearMiss)
+
+        val newNearMissIdx =
+          if nElves % 2 == 0 then
+            nearMissIdx
+          else
+            playfield(nearMissIdx).neighbour
+        reduce(turn = playfield(turn).neighbour, nElves - 1, newNearMissIdx)
+
+    reduce(turn = 1, nElves = nElves, nearMissIdx = nElves / 2)
 
   @main
   def day19(): Unit =
