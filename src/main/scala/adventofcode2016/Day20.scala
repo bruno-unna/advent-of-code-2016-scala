d3cfe5f57088249117e250e6e52f532248f13e11
Bruno Unna <bruno.unna@gmail.com>
2025-10-02 18:43:42 +0100

    Add scaladoc for day 20

--- src/main/scala/adventofcode2016/Day20.scala
+++ src/main/scala/adventofcode2016/Day20.scala
@@ -5,8 +5,24 @@ import zio.*
 import java.io.IOException
 import scala.collection.immutable.NumericRange
 
+/**
+ * Solves Advent of Code Day 20: Firewall Rules.
+ *
+ * This problem involves finding the first unblocked IP address and counting the total
+ * number of unblocked IP addresses given a list of blocked ranges.
+ */
 object Day20 extends ZIOAppDefault:
 
+  /**
+   * Solves Part 1: Finds the lowest-valued IP address that is not blocked.
+   *
+   * This is achieved by sorting the ranges and using `foldLeft` to track the
+   * current maximum blocked address (`current`). The first time the current address
+   * is less than a range's start, a gap is found and `current` is returned.
+   *
+   * @param ranges A vector of blocked IP address ranges.
+   * @return The first valid (unblocked) IP address as a [[Long]].
+   */
   def findFirstAllowedAddress(ranges: Vector[NumericRange[Long]]): Long =
     val sortedRanges = ranges.sorted
     sortedRanges.foldLeft(0L): (current, range) =>
@@ -14,6 +30,16 @@ object Day20 extends ZIOAppDefault:
       else if current < range.end then range.end + 1
       else current
 
+  /**
+   * Solves Part 2: Counts the total number of unblocked IP addresses.
+   *
+   * This is done by first merging all overlapping and contained ranges into a
+   * minimal set of non-overlapping ranges, and then subtracting the total
+   * blocked count from the total address space (2 to the power of 32).
+   *
+   * @param ranges A vector of blocked IP address ranges.
+   * @return The total count of valid (unblocked) IP addresses as a [[Long]].
+   */
   def countValidAddresses(ranges: Vector[NumericRange.Inclusive[Long]]): Long =
     val normalisedRanges =
       ranges
@@ -30,6 +56,11 @@ object Day20 extends ZIOAppDefault:
     val invalidAddressesCount = normalisedRanges.foldLeft(0L)((acc, range) => acc + (range.end - range.start + 1L))
     (1L << 32) - invalidAddressesCount
 
+  /**
+   * The main program entry point for Day 20.
+   *
+   * Reads firewall rules from a file, solves both parts of the problem, and prints the results.
+   */
   private def program =
     for
       rangesAsStrings <- Util.readStrings("/20.txt").map(_.toVector)
@@ -44,5 +75,7 @@ object Day20 extends ZIOAppDefault:
       _ <- Console.printLine(s"number of valid IP addresses: $validAddressesCount")
     yield ()
 
+  /**
+   * Defines the ZIO application run method.
+   */
   override def run: ZIO[Any, IOException | Option[Nothing], Unit] = program
-  
