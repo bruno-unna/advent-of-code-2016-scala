8a1f2f50731a36efa589ad7e07836cd4568fb3af
Bruno Unna <bruno.unna@gmail.com>
2025-10-03 20:04:39 +0100

    Define the ADT for the operations

--- src/main/scala/adventofcode2016/Day21.scala
+++ src/main/scala/adventofcode2016/Day21.scala
@@ -1,12 +1,33 @@
 package adventofcode2016
 
-import zio.{Console, ZIO, ZIOAppDefault, ZLayer}
+import zio.{Console, Task, ZIO, ZIOAppDefault, ZLayer}
 
 import java.io.IOException
 
 object Day21 extends ZIOAppDefault:
 
   object Scrambler:
+    sealed trait Operation:
+      def apply(input: String): String
+
+    case class SwapPositions(x: Int, y: Int) extends Operation:
+      override def apply(input: String): String = ???
+
+    case class SwapLetters(c: Char, c1: Char) extends Operation:
+      override def apply(input: String): String = ???
+
+    case class Rotate(x: Int) extends Operation:
+      override def apply(input: String): String = ???
+
+    case class RotateByIndex(x: Char) extends Operation:
+      override def apply(input: String): String = ???
+
+    case class Reverse(x: Int, y: Int) extends Operation:
+      override def apply(input: String): String = ???
+
+    case class Move(x: Int, y: Int) extends Operation:
+      override def apply(input: String): String = ???
+
     trait Service:
       def scramblePassword(password: String): ZIO[Any, String, String]
 
