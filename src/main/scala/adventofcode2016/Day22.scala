29ab2f45ff89b2e0564d6a67127f1918eb136820
Bruno Unna <bruno.unna@gmail.com>
2025-10-05 09:58:02 +0100

    Implement part 1 of day 22

---
+++ src/main/scala/adventofcode2016/Day22.scala
@@ -0,0 +1,45 @@
+package adventofcode2016
+
+import zio.{Console, UIO, ZIO, ZIOAppDefault}
+
+import java.io.IOException
+
+object Day22 extends ZIOAppDefault:
+
+  type Pos = (Int, Int)
+
+  case class Node(pos: Pos, size: Int, used: Int, available: Int, use: Int)
+
+  case object Node:
+    def parse(s: String): UIO[Option[Node]] =
+      val nodeRE = """^/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)%""".r
+      ZIO.succeed:
+        s match
+          case nodeRE(x, y, size, used, available, use) =>
+            Some(Node((x.toInt, y.toInt), size.toInt, used.toInt, available.toInt, use.toInt))
+          case _ =>
+            None
+
+  private def findViablePairs(nodes: Set[Node]): Set[(Pos, Pos)] =
+    for
+      a <- nodes
+      b <- nodes
+      if a != b
+      if a.used > 0
+      if a.used <= b.available
+    yield a.pos -> b.pos
+
+  private def program =
+    for
+      nodeStrings <- Util.readStrings("/22.txt")
+      potentialNodes <- ZIO.foreachPar(nodeStrings)(Node.parse)
+      nodes = potentialNodes.collect:
+        case Some(n) => n
+      viablePairs = findViablePairs(nodes.toSet)
+      _ <- Console.printLine(s"part 1: number of viable pairs: ${viablePairs.size}")
+    yield ()
+
+  /**
+   * The ZIO application run method, providing the live [[Scrambler.Service]].
+   */
+  override def run: ZIO[Any, IOException, Unit] = program
